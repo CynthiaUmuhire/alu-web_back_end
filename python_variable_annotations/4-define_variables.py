592236e777e7222ba5a708d327517dc2dfd5c6f9
CynthiaUmuhire <c.umuhire@alustudent.com>
2024-05-07 13:37:39 +0200

    feat: define variables

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,14 @@
+#!usr/bin/env python3
+
+"""
+define a function that takes in arguments with default values
+"""
+
+
+def define_variables(a: int = 1, pi: float = 3.14,
+                     i_understand_annotations: bool = "true",
+                     school: str = "Holberton") -> str:
+    """
+    return
+    """
+    return
