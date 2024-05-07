1f1bc85a3aa69d0de2b4afbdaeba28b5f1619db8
CynthiaUmuhire <c.umuhire@alustudent.com>
2024-05-07 11:42:32 +0200

    feat: float add function

---
+++ python_variable_annotations/add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+This is a type annotated function that adds two float numbers and return the sum as a float
+"""
+
+def add(a: float, b: float) -> float:
+    return a + b
+
+if __name__ == "__main__":
+    print(add(10.5, 20.5))  # 31.0
+    print(add(10.0, 20))  # 30.0
