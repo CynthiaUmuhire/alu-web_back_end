f69c8c85a8844ead0ff3dbad8c00975d9075ebd1
CynthiaUmuhire <c.umuhire@alustudent.com>
2024-05-08 14:53:07 +0200

    return a mixed type tuple

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -4,11 +4,12 @@ importing the Union module from the typing module
 """
 from typing import Union
 """
-function to_kv that takes a string k and an int OR float v as arguments and returns a tuple.
+function to_kv that takes a string k and an int OR float
+v as arguments and returns a mixed tuple.
 """
 
 
-def to_kv(k: str, v: Union[str, float]) -> tuple:
+def to_kv(k: str, v: Union[str, float]) -> tuple(str, float):
     """
     returns a tuple containing k and the square of v.
     """
