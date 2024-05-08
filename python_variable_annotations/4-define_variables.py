92da764a6ca30f3b6229b32b388b89653f465bdf
CynthiaUmuhire <c.umuhire@alustudent.com>
2024-05-08 18:40:01 +0200

    refactor

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python3
-""" Define variables"""
+
+"""
+define a function that takes in arguments with default values
+"""
+"""
+define variables
+"""
 a = 1
 pi = 3.14
 i_understand_annotations = True
 school = "Holberton"
 
-"""Function to annotate variables"""
+
+def define_variables(a: int = 1, pi: float = 3.14,
+                     i_understand_annotations: bool = "true",
+                     school: str = "Holberton"):
+    """
+    return a string with the default values
+    """
+    a = a
+    pi = pi
+    i_understand_annotations = i_understand_annotations
+    school = school
+    return a, pi, i_understand_annotations, school
 
 
-def annotate_variables():
-    """Annotate variables with their respective types"""
-    annotations = {
-        'a': (a, int),
-        'pi': (pi, float),
-        'i_understand_annotations': (i_understand_annotations, bool),
-        'school': (school, str)
-    }
-    """Return annotations"""
-    return annotations
+print(define_variables())
