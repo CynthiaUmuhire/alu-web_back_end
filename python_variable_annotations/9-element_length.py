e6a3483c4cb41192ac6c6fff465a0d48534c39a3
CynthiaUmuhire <c.umuhire@alustudent.com>
2024-05-08 18:23:33 +0200

    return a tuple with a sequence

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -10,7 +10,7 @@ integer.
 """
 
 
-def element_length(lst: Iterable[Sequence]) -> List[Tuple[int, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
     returns a list of tuples where each tuple contains a string and an
     integer.
