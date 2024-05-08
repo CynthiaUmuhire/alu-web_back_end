0a9ab37cadc973015ce44862cd07ad7ca9a32e80
CynthiaUmuhire <c.umuhire@alustudent.com>
2024-05-08 14:51:28 +0200

    change int to str

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -8,7 +8,7 @@ function to_kv that takes a string k and an int OR float v as arguments and retu
 """
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple:
+def to_kv(k: str, v: Union[str, float]) -> tuple:
     """
     returns a tuple containing k and the square of v.
     """
