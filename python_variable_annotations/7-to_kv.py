eabdfa3553b266b20be039c4c9666e192b4492ca
CynthiaUmuhire <c.umuhire@alustudent.com>
2024-05-08 14:50:22 +0200

    feat: return a mixed tuple

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+importing the Union module from the typing module
+"""
+from typing import Union
+"""
+function to_kv that takes a string k and an int OR float v as arguments and returns a tuple.
+"""
+
+
+def to_kv(k: str, v: Union[int, float]) -> tuple:
+    """
+    returns a tuple containing k and the square of v.
+    """
+    return (k, v**2)
