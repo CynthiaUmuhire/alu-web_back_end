793c7d103be36babdee99b135390deea8c3eb335
CynthiaUmuhire <c.umuhire@alustudent.com>
2024-05-08 08:46:57 +0200

    fix: use the typing list

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
+from typing import List
+
 """
 a function that gets a list of floats and returns their sum as a float
 """
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     return the sum of the list
     """
     return sum(input_list)
 
 
-if __name__:
-    "__name__"
 print(sum_list([1.0, 2.0, 3.0]))  # 6.0
