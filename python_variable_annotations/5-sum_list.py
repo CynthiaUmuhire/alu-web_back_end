327ad4c69a85bf2389843947b7945118a44ee638
CynthiaUmuhire <c.umuhire@alustudent.com>
2024-05-08 08:53:05 +0200

    format and document everything

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""
+import the List module from the typing library
+"""
 from typing import List
 
 """
@@ -11,6 +14,7 @@ def sum_list(input_list: List[float]) -> float:
     return the sum of the list
     """
     return sum(input_list)
+
 
 if __name__ == "__main__":
     print(sum_list([1.0, 2.0, 3.0]))  # 6.0
