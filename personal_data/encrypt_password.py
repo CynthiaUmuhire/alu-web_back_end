84668556de5eb64ab2dce82bdaf8d8d663ba8aaf
CynthiaUmuhire <c.umuhire@alustudent.com>
2024-05-24 12:33:49 +0200

    run lint

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
-""" encrypting password """
 
 """import the bycrypt module to encrypt the password"""
 
 
-
-
 import bcrypt
+
+
 def hash_password(password: str) -> bytes:
     """ encrypt password """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
