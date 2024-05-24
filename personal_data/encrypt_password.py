56297eea7b8c303f053752d01bcc0d471ba21058
CynthiaUmuhire <c.umuhire@alustudent.com>
2024-05-24 10:37:56 +0200

    feat: encrypt password and readme

---
+++ personal_data/encrypt_password.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" encrypting password """
+"""import the bycrypt module to encrypt the password"""
+
+
+
+
+import bcrypt
+def hash_password(password: str) -> bytes:
+    """ encrypt password """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ check if password is valid """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
